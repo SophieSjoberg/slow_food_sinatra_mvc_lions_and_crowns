4b0eebf06ebbfe79852fa0e138c34615b4280519
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-22 22:01:58 +0100

    WIP, adds _add_description_to_product.rb

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,5 +1,5 @@
-Given(/^there is a dish called "([^"]*)" with a price of "([^"]*)" kr$/) do |product_name, price|
-  Product.create(name: product_name, price: price.to_i)
+Given(/^there is a dish called "([^"]*)" with a price of "([^"]*)" kr and description of "([^"]*)"$/) do |product_name, price, description|
+  Product.create(name: product_name, price: price.to_i, description: description)
 end
 
 When(/^I visit the site$/) do
@@ -9,4 +9,8 @@ end
 Then(/^I would like to see "([^"]*)" with a price of "([^"]*)" kr$/) do |product_name, price|
   expected_output = "#{product_name} - #{price}"
   expect(page).to have_content expected_output
+end
+
+Then(/^I would like to see "([^"]*)"$/) do |description|
+  expect(page).to have_content products.description
 end
