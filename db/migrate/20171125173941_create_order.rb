4e628d3010a4771eb8b61d6fa4d9c0933099af64
aljoh <reachingalfred@gmail.com>
2017-11-25 21:56:00 +0100

    added specs for order_item

--- db/migrate/20171125173941_create_order.rb
+++ db/migrate/20171125173941_create_order.rb
@@ -1,7 +1,6 @@
 class CreateOrder < ActiveRecord::Migration[4.2]
   def change
-    create_table :orders do |t|
-      t.timestamps
+    create_table :orders do
     end
   end
 end
--- db/migrate/20171125174156_create_order_item.rb
+++ db/migrate/20171125174156_create_order_item.rb
@@ -1,7 +1,6 @@
 class CreateOrderItem < ActiveRecord::Migration[4.2]
   def change
-    create_table :order_items do |t|
-      t.timestamps
+    create_table :order_items do
     end
   end
 end
