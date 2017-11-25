2fb54afdf745af2c7a23cd8bc0559b39da5caafa
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-25 22:03:41 +0100

    refactoring

--- db/migrate/20171125173941_create_order.rb
+++ db/migrate/20171125173941_create_order.rb
@@ -1,6 +1,5 @@
 class CreateOrder < ActiveRecord::Migration[4.2]
   def change
-    create_table :orders do
-    end
+    create_table :orders
   end
 end
--- db/migrate/20171125174156_create_order_item.rb
+++ db/migrate/20171125174156_create_order_item.rb
@@ -1,6 +1,5 @@
 class CreateOrderItem < ActiveRecord::Migration[4.2]
   def change
-    create_table :order_items do
-    end
+    create_table :order_items
   end
 end
